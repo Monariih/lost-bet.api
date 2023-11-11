777eef0551d0cb1cb031739f8642be7272aa9584
Monariih <lucas.monari.lm@outlook.com>
2023-11-11 17:50:57 -0300

    update the Cors pollicy

--- src/main/java/com/lostbet/config/WebConfig.java
+++ src/main/java/com/lostbet/config/WebConfig.java
@@ -10,7 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("PUT", "DELETE", "GET", "POST");
     }
 
 }
